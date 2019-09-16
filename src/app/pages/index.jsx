b8d530ec9315f88ae0d316cbe3cfa00010ba44e7
Reed Davenport <reed@plusreed.com>
2019-09-16 10:08:07 -0400

    fix some oddities

--- src/app/pages/index.jsx
+++ src/app/pages/index.jsx
@@ -1,8 +1,8 @@
 import React, { Suspense } from 'react';
 
-const Header = React.lazy(() => import(/* webpackChunkName: "Header.component.dll" */'../components/Header'));
-const BinaryConverter = React.lazy(() => import(/* webpackChunkName: "Dec2Bin.component.dll" */'../components/BinaryConverter'));
-const DecimalConverter = React.lazy(() => import(/* webpackChunkName: "Bin2Dec.component.dll" */'../components/Bin2Dec'));
+const Header = React.lazy(() => import('../components/Header'));
+const BinaryConverter = React.lazy(() => import('../components/BinaryConverter'));
+const DecimalConverter = React.lazy(() => import('../components/Bin2Dec'));
 
 const Index = () => (
     <Suspense fallback={<h1>Waiting for ConversionCore...</h1>}>
