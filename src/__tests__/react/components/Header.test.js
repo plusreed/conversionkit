9c6b0a6526a6bb021ddd7c3ac9e3983833d70607
Reed <reed@plusreed.com>
2020-10-05 13:44:30 -0400

    Finish fallback tests, plus other things (#12)
    * Finish fallback test
    
    Also adds a text prop to Header and updates its tests.
    
    * Add new test to Header
    
    * Remove unused test
    
    * remove todo from test

--- src/__tests__/react/components/Header.test.js
+++ src/__tests__/react/components/Header.test.js
@@ -7,3 +7,15 @@ it('renders without crashing', () => {
   ReactDOM.render(<Header />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('contains text when text prop is passed', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Header text={"hey"} />, div)
+  expect(div.innerHTML).toMatch(/hey/)
+})
+
+it('says \'ConversionKit\' when no text is provided', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Header />, div)
+  expect(div.innerHTML).toMatch(/ConversionKit/)
+})
--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Header = () => (
+const Header = props => (
     <>
         <div className="h-auto p-2 bg-gray-900 pl-4">
-            <h1 className="font-bold text-white text-xl">ConversionKit</h1>
+            <h1 className="font-bold text-white text-xl">{props.text || "ConversionKit"}</h1>
         </div>
     </>
 );
