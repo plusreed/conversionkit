b63733f010df82684e8498c7c559aaf76df4ecfb
Reed <reed@plusreed.com>
2020-10-19 15:50:40 -0400

    Add debugging features (#14)
    - Adds `react-test-renderer` for unit tests
    - Use snapshots along with existing tests
    - Add launch.json for easier VSCode debugging

--- src/__tests__/react/pages/WhyUse.test.js
+++ src/__tests__/react/pages/WhyUse.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'
 import WhyUse from '../../../app/pages/WhyUse';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<WhyUse />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('matches snapshot', () => {
+  const tree = renderer
+    .create(<WhyUse />)
+    .toJSON()
+
+  expect(tree).toMatchSnapshot()
 });
